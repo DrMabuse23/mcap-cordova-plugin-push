20897dabef0884dd97c8d97a83f69ed99ebcccae
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-04-16 13:38:24 +0200

    next try

--- www/underscore_wrapper.js
+++ www/underscore_wrapper.js
@@ -4,3 +4,6 @@ cordova.define('underscore', function (localRequire, exports, module) {
 cordova.define('mCap', function (localRequire, exports, module) {
     module.exports = window.mCap;
 });
+cordova.define('Backbone', function (localRequire, exports, module) {
+    module.exports = window.Backbone;
+});
