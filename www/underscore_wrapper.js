0d458d7af27bee8bb23c144726bb15737914cb69
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-04-16 12:44:02 +0200

    mCap is undefined

--- www/underscore_wrapper.js
+++ www/underscore_wrapper.js
@@ -1,3 +1,6 @@
 cordova.define('underscore', function (localRequire, exports, module) {
     module.exports = window._;
 });
+cordova.define('mCap', function (localRequire, exports, module) {
+    module.exports = window.mCap;
+});
